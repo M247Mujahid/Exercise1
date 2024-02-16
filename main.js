9308b76faa73de9eaf7643349d1c90a2125efd2d
M247Mujahid <159254265+M247Mujahid@users.noreply.github.com>
2024-02-16 11:44:40 +0500

    Exercise #2 "Hello World"

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Mujahid";
+let message = `Hello ${personName}, hope you are doing well.`;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let personName: string = "Mujahid"
+let message: string = `Hello ${personName}, hope you are doing well.`
+console.log(message)
